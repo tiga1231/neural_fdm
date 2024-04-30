61eee50f9dce11ce34c2ab817e2c9ea25d02a5cf
Rafael Pastrana <arpj@princeton.edu>
2024-04-30 09:50:44 -0400

    Refactored `train.py` into 2 functions

--- scripts/train.py
+++ scripts/train.py
@@ -26,7 +26,7 @@ from neural_fofin.serialization import save_model
 # Script function
 # ===============================================================================
 
-def train(model, save=True, config="config"):
+def train(model, config="config", plot=True, save=True):
     """
     Train a model to approximate a family of arbitrary shapes with mechanically-feasible geometries.
 
@@ -35,81 +35,40 @@ def train(model, save=True, config="config"):
     model: `str`
         The model name.
         Supported models are formfinder, autoencoder, and piggy.
-    save: `bool`
-        If `True`, save the trained model and the loss histories.
     config: `str`
         The filepath (without extension) of the YAML config file with the training hyperparameters.
+    plot: `bool`
+        If `True`, plot the loss curves.
+    save: `bool`
+        If `True`, save the trained model and the loss histories.
     """
     MODEL_NAME = model
     SAVE = save
+    PLOT = plot
     CONFIG_NAME = config
 
     # load yaml file with hyperparameters
     with open(f"{CONFIG_NAME}.yml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
-    # unpack parameters
-    seed = config["seed"]
-    training_params = config["training"]
-    batch_size = training_params["batch_size"]
-    steps = training_params["steps"]
-    loss_params = config["loss"]
-
-    # randomness
-    key = jrn.PRNGKey(seed)
-    model_key, generator_key = jax.random.split(key, 2)
-
-    # create experiment
-    generator = build_data_generator(config)
-    structure = build_connectivity_structure_from_generator(generator)
-    optimizer = build_optimizer(config)
-    model = build_neural_model(MODEL_NAME, config, generator, model_key)
-    print(f"\nTraining {MODEL_NAME}")
-
-    # sample initial data batch
-    xyz = vmap(generator)(jrn.split(generator_key, batch_size))
-
-    # warmstart
-    start_loss = compute_loss(model, structure, xyz, loss_params)
-    print(f"{loss_params=}")
-    print(f"{MODEL_NAME} start loss: {start_loss:.6f}")
-
-    # train models
-    print("\nTraining")
-
-    start = time.perf_counter()
-    train_data = train_model(
-        model,
-        structure,
-        optimizer,
-        generator,
-        loss_params=loss_params,
-        num_steps=steps,
-        batch_size=batch_size,
-        key=generator_key,
-        callback=None
-        )
-    end = time.perf_counter()
-
+    # train model
+    train_data = train_model_from_config(model, config, callback=None)
     trained_model, trained_opt_states, loss_history = train_data
-    print("\nTraining completed")
-    print(f"Training time: {end - start:.4f} s")
-
-    end_loss = compute_loss(trained_model, structure, xyz, loss_params)
-    print(f"{MODEL_NAME} last loss: {end_loss}")
 
     # plot loss curves
-    print("\nPlotting")
-    loss_labels = ["loss", "shape", "residual"]
-    plot_smoothed_losses(loss_history,
-                         window_size=50,
-                         labels=loss_labels)
+    if PLOT:
+        print("\nPlotting")
+        loss_labels = ["loss", "shape", "residual"]
+        plot_smoothed_losses(loss_history,
+                             window_size=50,
+                             labels=loss_labels)
 
     # save models
     if SAVE:
         print("\nSaving results")
 
         _filename = MODEL_NAME
+        loss_params = config["loss"]
         if loss_params["residual"]["include"] > 0 and MODEL_NAME != "formfinder":
             _filename += "_pinn"
 
@@ -127,6 +86,79 @@ def train(model, save=True, config="config"):
                     file.write(f"{_value}\n")
 
             print(f"Saved loss history to {_filepath}")
+
+
+# ===============================================================================
+# Helper functions
+# ===============================================================================
+
+def train_model_from_config(model, config, callback=None):
+    """
+    Train a model to approximate a family of arbitrary shapes with mechanically-feasible geometries.
+
+    Parameters
+    ___________
+    model: `str`
+        The model name.
+        Supported models are formfinder, autoencoder, and piggy.
+    config: `dict`
+        A dictionary with the hyperparameters configuration.
+        If `True`, save the trained model and the loss histories.
+    callback: `Callable`
+        A callback function to call at every train step.
+    """
+    MODEL_NAME = model
+
+    # unpack parameters
+    seed = config["seed"]
+    training_params = config["training"]
+    batch_size = training_params["batch_size"]
+    steps = training_params["steps"]
+    loss_params = config["loss"]
+
+    # randomness
+    key = jrn.PRNGKey(seed)
+    model_key, generator_key = jax.random.split(key, 2)
+
+    # create experiment
+    print(f"\nTraining {MODEL_NAME}")
+    generator = build_data_generator(config)
+    structure = build_connectivity_structure_from_generator(generator)
+    optimizer = build_optimizer(config)
+    model = build_neural_model(MODEL_NAME, config, generator, model_key)
+
+    # sample initial data batch
+    xyz = vmap(generator)(jrn.split(generator_key, batch_size))
+
+    # warmstart
+    start_loss = compute_loss(model, structure, xyz, loss_params)
+    print(f"{MODEL_NAME} start loss: {start_loss:.6f}")
+
+    # train models
+    print("\nTraining")
+    start = time.perf_counter()
+    train_data = train_model(
+        model,
+        structure,
+        optimizer,
+        generator,
+        loss_params=loss_params,
+        num_steps=steps,
+        batch_size=batch_size,
+        key=generator_key,
+        callback=callback
+        )
+    end = time.perf_counter()
+
+    print("\nTraining completed")
+    print(f"Training time: {end - start:.4f} s")
+
+    trained_model, trained_opt_states, loss_history = train_data
+
+    end_loss = compute_loss(trained_model, structure, xyz, loss_params)
+    print(f"{MODEL_NAME} last loss: {end_loss}")
+
+    return train_data
 
 
 # ===============================================================================
