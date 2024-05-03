017b33d1908e1f2b5cbe70bf054493919644825d
Rafael Pastrana <arpj@princeton.edu>
2024-05-03 09:44:43 -0400

    Implemented smoothness error term

--- src/neural_fofin/losses.py
+++ src/neural_fofin/losses.py
@@ -7,28 +7,60 @@ from neural_fofin.models import AutoEncoderPiggy
 from neural_fofin.helpers import vertices_residuals_from_xyz
 
 
-def compute_loss(model, structure, x, loss_params, aux_data=False, piggy_mode=True):
+def compute_loss(
+        model,
+        structure,
+        x,
+        loss_fn,
+        loss_params,
+        aux_data=False,
+        piggy_mode=False
+):
     """
     Compute the model loss according to the model type.
     """
     predict_fn = vmap(model, in_axes=(0, None, None, None))
     x_hat, data_hat = predict_fn(x, structure, True, piggy_mode)
 
-    loss_fn = _compute_loss
+    # TODO: make _loss_fn an input, and make isinstance check before running this function
+    _loss_fn = _compute_loss
     if isinstance(model, AutoEncoderPiggy):
-        loss_fn = _compute_loss_piggy
+        _loss_fn = _compute_loss_piggy
 
-    return loss_fn(x, x_hat, data_hat, structure, loss_params, aux_data, piggy_mode)
+    return _loss_fn(loss_fn, loss_params, x, x_hat, data_hat, structure, aux_data, piggy_mode)
 
 
-def _compute_loss_piggy(x, x_data_hat, y_data_hat, structure, loss_params, aux_data, piggy_mode=True):
+def _compute_loss(
+        loss_fn,
+        loss_params,
+        x,
+        x_hat,
+        params_hat,
+        structure,
+        aux_data,
+        piggy_mode=False
+):
+    """
+    Compute the model loss of an autoencoder.
+    """
+    return loss_fn(
+        x,
+        x_hat,
+        params_hat,
+        structure,
+        loss_params,
+        aux_data
+    )
+
+
+def _compute_loss_piggy(loss_fn, loss_params, x, x_data_hat, y_data_hat, structure, aux_data, piggy_mode=True):
     """
     Compute the model loss of a piggy autoencoder.
     """
     x_hat, x_params_hat = x_data_hat
 
     if not piggy_mode:
-        loss_data = _compute_loss(
+        loss_data = loss_fn(
             x,
             x_hat,
             x_params_hat,
@@ -40,7 +72,7 @@ def _compute_loss_piggy(x, x_data_hat, y_data_hat, structure, loss_params, aux_d
         y_hat, y_params_hat = y_data_hat
         q, xyz_fixed, loads = y_params_hat
 
-        loss_data = _compute_loss(
+        loss_data = loss_fn(
             x_hat,
             y_hat,
             y_params_hat,
@@ -52,7 +84,66 @@ def _compute_loss_piggy(x, x_data_hat, y_data_hat, structure, loss_params, aux_d
     return loss_data
 
 
-def _compute_loss(x, x_hat, params_hat, structure, loss_params, aux_data, *args):
+def compute_loss_shape_residual_smoothness(x, x_hat, params_hat, structure, loss_params, aux_data, *args):
+    """
+    Compute the model loss.
+
+    Parameters
+    ----------
+    x : target
+    x_hat : prediction
+    params_hat : parameters prediction (aux data)
+    structure : the connectivity graph of the structure
+    loss_params : the scaling parameters to combine the loss' error terms
+    aux_data : if true, returns auxiliary data
+    """
+    shape_params = loss_params["shape"]
+    factor_shape = shape_params["weight"] / shape_params["scale"]
+
+    # select only points on rings to compute shape error
+    dims = shape_params["dims"]
+    indices = shape_params["indices"]
+
+    def reshape_shape(_x):
+        return jnp.reshape(_x, dims)[indices, :].ravel()
+
+    x_hat_slice = vmap(reshape_shape)(x_hat)
+
+    loss_shape = compute_error_shape(x, x_hat_slice)
+    loss_shape = factor_shape * loss_shape
+
+    residual_params = loss_params["residual"]
+    factor_residual = residual_params["weight"] / residual_params["scale"]
+    loss_residual = compute_error_residual(x_hat, params_hat, structure)
+    loss_residual = factor_residual * loss_residual
+
+    smooth_params = loss_params["energy"]
+    factor_smooth = smooth_params["weight"] / smooth_params["scale"]
+    loss_smooth = compute_error_smoothness(x_hat, params_hat, structure)
+    loss_smooth = factor_smooth * loss_smooth
+
+    loss = 0.0
+    if shape_params["include"]:
+        loss = loss + loss_shape
+    if residual_params["include"]:
+        loss = loss + loss_residual
+    if smooth_params["include"]:
+        loss = loss + loss_smooth
+
+    loss_terms = (
+        loss,
+        loss_shape,
+        loss_residual,
+        loss_smooth
+    )
+
+    if aux_data:
+        return loss, loss_terms
+
+    return loss
+
+
+def compute_loss_shape_residual(x, x_hat, params_hat, structure, loss_params, aux_data, *args):
     """
     Compute the model loss.
 
@@ -130,3 +221,57 @@ def compute_error_residual(x_hat, params_hat, structure):
     batch_error = jnp.sqrt(jnp.sum(error, axis=-1))
 
     return jnp.mean(batch_error, axis=-1)
+
+
+def compute_error_smoothness(x_hat, params_hat, structure):
+    """
+    Calculate the shape smoothness (fairness) error.
+    """
+    error = vmap(vertices_fairness, in_axes=(0, None))(x_hat, structure)
+    batch_error = jnp.sqrt(jnp.sum(error, axis=-1))
+
+    return jnp.mean(batch_error, axis=-1)
+
+
+def vertices_fairness(xyz, structure):
+    """
+    Compute the shape fairness energy of the vertices of a structure.
+    """
+    xyz = jnp.reshape(xyz, (-1, 3))
+
+    def vertex_fairness(xyz_vertex, adjacency_vertex):
+        """
+        Compute the fairness of an n-gon vertex neighborhood.
+        """
+        xyz_nbrs = adjacency_vertex @ xyz / jnp.sum(adjacency_vertex, axis=-1)
+
+        fvector = xyz_vertex - xyz_nbrs
+        assert fvector.shape == xyz_vertex.shape
+
+        return jnp.sum(jnp.square(fvector))
+
+    indices = structure.indices_free
+    adjacency_free = structure.adjacency[indices, :]
+    xyz_free = xyz[indices, :]
+
+    vertices_fairness_fn = vmap(vertex_fairness)
+
+    return vertices_fairness_fn(xyz_free, adjacency_free)
+
+
+def print_loss_summary(loss_terms, labels=None, prefix=None):
+    """
+    """
+    if not labels:
+        labels = ["Loss", "Shape error", "Residual error", "Smooth error"]
+
+    msg_parts = []
+    if prefix:
+        msg_parts.append(prefix)
+
+    for term, label in zip(loss_terms, labels):
+        part = f"{label}: {term:.4f}"
+        msg_parts.append(part)
+
+    msg = "\t".join(msg_parts)
+    print(msg)
