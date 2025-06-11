98ba9dcbedda7ea937eff2c57b4e69ab81d99047
Rafael Pastrana <arpj@princeton.edu>
2025-06-11 13:38:33 -0400

    [Scripts] Created file with camera configurations

---
+++ scripts/camera.py
@@ -0,0 +1,24 @@
+"""
+Camera configurations to view the outputs of the bezier and tower tasks.
+"""
+
+CAMERA_CONFIG_BEZIER = {    
+    "position": (30.34, 30.28, 42.94),
+    "target": (0.956, 0.727, 1.287),
+    "distance": 20.0,
+}
+
+CAMERA_CONFIG_BEZIER_TOP = {    
+    "position": (0.3, 0.85, 7.5),
+    "target": (0.3, 0.85, 0.000),
+    "distance": 7.5,
+    "rotation": (0.000, 0.000, 0.000),
+    "use_top_view": True
+}
+
+CAMERA_CONFIG_TOWER = {    
+    "position": (10.718, 10.883, 14.159),
+    "target": (-0.902, -0.873, 3.846),
+    "distance": 19.482960680274577,
+    "rotation": (1.013, 0.000, 2.362),
+}
