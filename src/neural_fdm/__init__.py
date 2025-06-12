b0e8ace03972230f1583e12b37f407b5c86be314
Rafael Pastrana <arpj@princeton.edu>
2025-06-12 16:23:05 -0400

    [Dependencies] Monkey patched numpy version for `compas_view==0.7.0`

--- src/neural_fdm/__init__.py
+++ src/neural_fdm/__init__.py
@@ -1,8 +1,12 @@
 import os
-
+import numpy as np
 
 HERE = os.path.dirname(__file__)
 HOME = os.path.abspath(os.path.join(HERE, "../../"))
 DATA = os.path.abspath(os.path.join(HOME, "data"))
 FIGURES = os.path.abspath(os.path.join(HOME, "figures"))
 SCRIPTS = os.path.abspath(os.path.join(HOME, "scripts"))
+
+# Monkey patch numpy for compas_view2==0.7.0
+if not hasattr(np, 'int'):
+    np.int = np.int64  # noqa: F821
